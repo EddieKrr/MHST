98592f4bffeb825a80df24b0220bc9975e7b9d01
EddieKrr <166714763+EddieKrr@users.noreply.github.com>
2025-06-30 15:34:57 +0300

    Updated site colors

--- header.php
+++ header.php
@@ -5,14 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
     <link rel="stylesheet" href="CSS/head.css">
-    <link rel="stylesheet" href="CSS/light-mc.css">
 </head>
 <body >
     <header>
         <div class="nav">
             <div class="logo">
                 <a href="index.html">
-                    <img src="images/logo.png" alt="Logo">
+                    <img src="images/logo.jpg" alt="Logo">
                 </a>
             </div>
             <ul class="nav-links">
