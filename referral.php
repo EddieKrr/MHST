ed3ed4fc2185ddfe0a52f05efbb92e1b794af963
EddieKrr <166714763+EddieKrr@users.noreply.github.com>
2025-07-14 07:58:18 +0300

    Added contact info to referral page

--- referral.php
+++ referral.php
@@ -11,9 +11,9 @@
     <div class="refP">
         <div class="sec2">
            <div> <img src="images/user.svg" alt="User Icon" class ="user-icon"></div>
-            <h2> John Doe</h2>
-            <h3>Specialty: BPD</h3>
-            <a href="mailto:johndoe@example.com"><h4>Contact: johndoe@example.com</h4></a>
+            <h2> Austin O</h2>
+            <h3>Specialty: General</h3>
+            <a href="https://wa.me/254729558635?text=Hello!%20I%20have%20been%20referred%20by%20the%20MHST.%20When%20are%20you%20available%20to%20set%20up%20an%20appointment?" target="_blank">Send Message</a>
         </div>
         <div class="sec2">
             <div> <img src="images/user.svg" alt="User Icon" class ="user-icon"></div>
